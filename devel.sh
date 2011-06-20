8ef69f9095b9e4e3daeb101d334df9bd9076ee8d
Jonathan Melnick <jonathan.melnick@gmail.com>
2011-06-20 21:14:06 +0800

    Add devel shell wrapper and git-add script

---
+++ devel.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#
+# Vars: handler and command
+#
+
+DEVEL_SH_HANDLER=$1
+DEVEL_SH_COMMAND=$2
+
+if [ ! -n "$DEVEL_SH_HANDLER" ]
+then
+	echo "Enter handler:"
+	read DEVEL_SH_HANDLER
+fi
+
+if [ ! -n "$DEVEL_SH_COMMAND" ]
+then
+	echo "Enter command:"
+	read DEVEL_SH_COMMAND
+fi
+
+#
+# Path
+#
+DEVEL_SH_PATH="./shell/devel/$DEVEL_SH_HANDLER/$DEVEL_SH_COMMAND.sh"
+
+if [ ! -f $DEVEL_SH_PATH ]
+then
+	echo "ERROR: Handler or command doesn't exist! ($DEVEL_SH_PATH)"
+	exit
+fi
+
+#
+# Source the include to use current shell
+#
+. $DEVEL_SH_PATH
---
+++ shell/devel/git/add.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+git add -f app/code/community/Dhmedia/Devel/
+git add -f app/code/local/Mage/Core/Block/Template.php
+git add -f app/design/frontend/default/default/template/devel/
+git add -f app/design/frontend/default/default/layout/devel/
+git add -f app/etc/modules/Dhmedia_Devel.xml
+git add -f js/codemirror/
+git add -f js/devel/
+git add -f lib/krumo/
+git add -f js/devel/
+git add -f shell/devel/
+git add -f skin/frontend/default/default/devel/
+git add -f .gitignore
+git add -f devel.sh
+git status
